a478bbb867d0211093853cee6f6d722325e08b43
John Brunton <john_brunton@hotmail.co.uk>
2014-01-19 13:19:25 +0000

    moved ShellSupport into a separate file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,28 +1,6 @@
 require 'heroploy/heroku_commands'
 require 'heroploy/git_commands'
-
-module ShellSupport
-  def stub_shell
-    Shell.stub(:eval).and_return("")
-    Shell.stub(:exec)
-  end
-  
-  def expect_command(cmd = nil)
-    if cmd.nil?
-      expect(Shell).to receive(:exec)
-    else
-      expect(Shell).to receive(:exec).with(cmd)
-    end
-  end
-  
-  def expect_eval(cmd = nil)
-    if cmd.nil?
-      expect(Shell).to receive(:eval)
-    else
-      expect(Shell).to receive(:eval).with(cmd)
-    end
-  end
-end
+require 'support/shell_support'
 
 RSpec.configure do |config|
   config.color_enabled = true
