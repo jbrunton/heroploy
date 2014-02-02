387903952f3088bb1a8cd390eb64bf80eb115b77
John Brunton <john_brunton@hotmail.co.uk>
2014-02-02 17:22:43 +0000

    #37 added docs for DeploymentConfig class and Heroploy module

--- lib/heroploy/config/deployment_config.rb
+++ lib/heroploy/config/deployment_config.rb
@@ -6,16 +6,24 @@ module Heroploy
     class DeploymentConfig
       attr_accessor :environments
   
+      # Returns the {Environment} instance for the given name, or nil if none is found
+      #
+      # @param env_name [String] the name of the {Environment} to return
+      # @return [Environment, nil] the {Environment} instance with the given name, or nil if none is found
       def [](env_name)
         environments.select{ |env| env.name == env_name }.first
       end
   
+      # Initializes {#environments} with an {Environment} instance for each specified environment in the given hash
+      #
+      # @param attrs [Hash] YAML representation of the deployment configuration
       def initialize(attrs = {})
         unless attrs['environments'].nil?
           @environments = attrs['environments'].map { |name, attrs| Environment.new(name, attrs) }
         end
       end
   
+      # Instantiates and returns a new instance of {DeploymentConfig DeploymentConfig} initialized with the YAML config file found at 'config/heroploy.yml'
       def self.load
         DeploymentConfig.new(YAML::load(File.open('config/heroploy.yml')))
       end
