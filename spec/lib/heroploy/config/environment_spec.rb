7c4288dc731d6e8bb4dedfc02cfd362b6c50c1fe
John Brunton <john_brunton@hotmail.co.uk>
2014-02-02 21:29:48 +0000

    #37 added specs for Environment class

---
+++ spec/lib/heroploy/config/environment_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe Heroploy::Config::Environment do
+  let(:env_name) { 'production' }
+
+  describe "#initialize" do
+    context "it initializes the accessors" do
+      let(:remote_name) { 'my-remote' }
+      let(:app_name) { 'my-app' }
+      let(:tag) { 'my-tag' }
+      let(:attrs) { {'remote' => remote_name, 'app' => app_name, 'tag' => tag} }
+      
+      subject(:environment) { Heroploy::Config::Environment.new(env_name, attrs) }
+
+      its(:name) { should eq(env_name) }
+      its(:remote) { should eq(remote_name) }
+      its(:app) { should eq(app_name) }
+      its(:tag) { should eq(tag) }
+      its(:checks) { should be_instance_of(Heroploy::Config::EnvironmentChecks) }      
+    end
+    
+    context "if no remote name is given" do
+      subject(:environment) { Heroploy::Config::Environment.new(env_name, {}) }
+      
+      it "uses the environment name for the remote" do
+        expect(environment.remote).to eq(env_name)
+      end
+    end
+  end
+end
