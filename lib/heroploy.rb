dac66adb1e4ad51307929e4ac9ba4a809b3c8e65
John Brunton <john_brunton@hotmail.co.uk>
2014-02-02 21:08:13 +0000

    #37 removed documentation

--- lib/heroploy.rb
+++ lib/heroploy.rb
@@ -4,9 +4,6 @@ require "heroploy/version"
 require "heroploy/engine" if defined?(Rails) && Rails::VERSION::MAJOR >= 3
 
 module Heroploy
-  # The root path of the Heroploy gem on the file system
-  #
-  # @return [Pathname] the root path of the gem
   def self.root
     Pathname.new(File.expand_path '../..', __FILE__)
   end
