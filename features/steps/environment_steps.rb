96c965b13f11bd97461ad688a0e3bb5d6eed6771
John Brunton <john_brunton@hotmail.co.uk>
2014-03-07 20:47:00 +0000

    #83 refactored cucumber steps

--- features/steps/environment_steps.rb
+++ features/steps/environment_steps.rb
@@ -1,34 +1,32 @@
 Given(/^a (development|staging|production) environment$/) do |env_type|
   @environments ||= []
-  @environments << build(:environment, env_type.to_sym)
-  
-  allow_any_instance_of(Heroploy::Tasks::CheckTaskLib).to receive(:git_remote_exists?).
-    with(env_type).
-    and_return(true)
-
-  allow_any_instance_of(Heroploy::Tasks::CheckTaskLib).to receive(:git_remote_has_branch?).
-    and_return(true)
-
-  allow_any_instance_of(Heroploy::Tasks::CheckTaskLib).to receive(:git_remote_behind?).
-    and_return(false)
+  @environments << build(:environment, env_type.to_sym)  
 end
 
-When(/^I run "(.*?)"$/) do |task_name|
-  Rake::Task.clear
+def init_tasks
+  deployment_config = build(:deployment_config, environments: @environments)
+  Heroploy::Tasks::DeployTaskLib.new(deployment_config)
+end
 
-  @deployment_config = build(:deployment_config, environments: @environments)
-  Heroploy::Tasks::DeployTaskLib.new(@deployment_config)
+def stub_tasks
+  Rake::Task.tasks.each do |task|
+    allow(task).to receive(:execute).and_call_original
+  end
+end
 
+def stub_travis
   @travis_repo = build(:travis_repo)
   Travis::Repository.stub(:find).and_return(@travis_repo)
 
   @travis_build ||= build(:travis_build, :passed)
-  allow(@travis_repo).to receive(:branch).and_return(@travis_build)
-  
-  Rake::Task.tasks.each do |task|
-    allow(task).to receive(:execute).and_call_original
-  end
+  allow(@travis_repo).to receive(:branch).and_return(@travis_build)  
+end
 
+When(/^I run "(.*?)"$/) do |task_name|
+  init_tasks
+  stub_travis
+  stub_tasks
+  
   begin
     Rake::Task[task_name].invoke
   rescue => error
@@ -37,16 +35,21 @@ When(/^I run "(.*?)"$/) do |task_name|
 end
 
 Then(/^heroploy should execute "(.*?)"$/) do |command|
-  expect(Heroploy::Shell).to have_received(:exec).with(command)
+  expect(Heroploy::Shell).
+    to have_received(:exec).
+    with(command)
 end
 
 Then(/^heroploy should invoke "(.*?)"$/) do |task_name|
-  expect(Rake::Task[task_name]).to have_received(:execute)
+  expect(Rake::Task[task_name]).
+    to have_received(:execute)
 end
 
 When(/^I have "(.*?)" checked out$/) do |branch_name|
   @branch_name = branch_name
-  allow_any_instance_of(Heroploy::Tasks::CheckTaskLib).to receive(:current_branch).and_return(branch_name)
+  allow_any_instance_of(Heroploy::Tasks::CheckTaskLib).
+    to receive(:current_branch).
+    and_return(branch_name)
 end
 
 Given(/^the travis build is failing$/) do
@@ -65,13 +68,15 @@ Then(/^the task should fail with "(.*?)"$/) do |expected_error|
 end
 
 When(/^my branch is ahead of origin$/) do
-  allow_any_instance_of(Heroploy::Tasks::CheckTaskLib).to receive(:git_remote_behind?).
+  allow_any_instance_of(Heroploy::Tasks::CheckTaskLib).
+    to receive(:git_remote_behind?).
     with('origin', @branch_name).
     and_return(true)
 end
 
 When(/^my branch is ahead of staging$/) do
-  allow_any_instance_of(Heroploy::Tasks::CheckTaskLib).to receive(:git_remote_behind?).
+  allow_any_instance_of(Heroploy::Tasks::CheckTaskLib).
+    to receive(:git_remote_behind?).
     with('staging', 'master', @branch_name).
     and_return(true)
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -14,6 +14,17 @@ World(FactoryGirl::Syntax::Methods)
 Before do
   allow(Heroploy::Shell).to receive(:eval).and_return("")
   allow(Heroploy::Shell).to receive(:exec)
+  
+  allow_any_instance_of(Heroploy::Tasks::CheckTaskLib).to receive(:git_remote_exists?).
+    and_return(true)
+
+  allow_any_instance_of(Heroploy::Tasks::CheckTaskLib).to receive(:git_remote_has_branch?).
+    and_return(true)
+
+  allow_any_instance_of(Heroploy::Tasks::CheckTaskLib).to receive(:git_remote_behind?).
+    and_return(false)
+
+  Rake::Task.clear
 end
 
 
